28159d05d2fd0fc7da78c930e0774474f6d08bbf
gbryant-dev <gbryant1@hotmail.co.uk>
2021-04-25 23:59:40 +0100

    Add user and group functions to Security Service

--- src/services/security-service.ts
+++ src/services/security-service.ts
@@ -1,3 +1,4 @@
+import { Group } from "../models/group";
 import { User } from "../models/user";
 import RestService from "./rest-service";
 
@@ -23,22 +24,71 @@ class SecurityService {
     return User.fromJson(response);
   }
 
+  // createUser
+  async createUser(user: User) {
+    return this.http.POST(`/api/v1/Users('${user.name}')`, user.body);
+  }
+
   // updateUser
+  async updateUser(user: User) {
+    return this.http.PATCH(`/api/v1/Users('${user.name}')`, user.body);
+  }
 
   // deleteUser
+  async deleteUser(user: User) {
+    return this.http.DELETE(`/api/v1/Users('${user.name}')`);
+  }
 
   // getAllGroups
+  async getAllGroups () {
+    const response = this.http.GET('/api/v1/Groups');
+    return response['value'].map(group => Group.fromJson(group));
+  }
 
   // getGroup
+  async getGroup (groupName: string) {
+    const response = this.http.GET(`/api/v1/Groups('${groupName}')`);
+    return Group.fromJson(response);
+  }
+
+  async getUserGroups (userName: string): Promise<Group[]> {
+    const response = this.http.GET(`/api/v1/Users('${userName}')/Groups`);
+    return response['value'].map((group: Group) => Group.fromJson(group));
+  }
+
+  // createGroup
+  async createGroup (group: Group) {
+    return this.http.POST('/api/v1/Groups', group.body);
+  }
 
   // updateGroup
+  async updateGroup (group: Group) {
+    return this.http.PATCH(`/api/v1/Group('${group.name}')`, group.body);
+  }
 
   // deleteGroup
+  async deleteGroup (groupName: string) {
+    return this.http.DELETE(`/api/v1/Groups('${groupName}')`);
+  }
 
   // addUserToGroup
+  async addUserToGroups (user: User, groups: string[]) {
+    const currentGroups = await this.getUserGroups(user.name);
+    
+    const body = {
+      'Groups@odata.bind': [
+        ...currentGroups.map((group: Group) => `Groups('${group.name}')`),
+        ...groups.map((group: string) => `Groups('${group}')`)
+      ]
+    }
+    return this.http.PATCH(`/api/v1/Users('${user.name}')`, body);
+  }
   
   // removeUserFromGroup
-
+  async removeUserFromGroup(userName: string, groupName: string) {
+    const url = `/api/v1/Users('${userName}')/Groups?$id=Groups('${groupName}')`;
+    return this.http.DELETE(url);
+  }
 }
 
 
