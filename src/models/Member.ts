541a0eb075bfb15c5f54eaab40b068567ceebb03
gbryant-dev <gbryant1@hotmail.co.uk>
2021-06-26 20:26:41 +0100

    Add Member and cell related models

--- src/models/Member.ts
+++ src/models/Member.ts
@@ -1,3 +1,29 @@
 class Member {
-    
+  public name: string;
+  public uniqueName: string;
+  public type: string;
+  public ordinal: number;
+  public weight: number;
+  public attributes: {[key: string]: string | number };
+  public hierarchyName: string;
+  public level: number;
+
+
+  constructor() {
+
+  }
+
+  static fromJson(data: any) {
+    return new Member()
+  }
+
+  get body() {
+    return this.constructBody()
+  }
+
+  constructBody() {
+    const body = {}
+
+    return body;
+  }
 }
---
+++ src/models/cellset.ts
@@ -0,0 +1,16 @@
+
+class Cellset {
+ 
+
+  constructor() {
+
+  }
+
+
+  static fromJson() {
+
+  }
+
+}
+
+export default Cellset;
