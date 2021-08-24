c014fbc8f0c1ef2987e509f976d00f88eed6fdf9
gbryant-dev <gbryant1@hotmail.co.uk>
2021-08-24 23:32:53 +0100

    Add more tests for ElementService

--- test/services/element-service.test.ts
+++ test/services/element-service.test.ts
@@ -51,11 +51,56 @@ describe('ElementService', () => {
     expect(elements).toHaveLength(2);
   })
 
-  it.todo('Should fetch all element names in a hierarchy')
-  it.todo('Should create a new element in a hierarchy')
-  it.todo('Should update an element in a hierarchy')
-  it.todo('Should delete an element from a hierarchy')
-  it.todo('Should should fetch all leaf level elements in a hierarchy')
-  it.todo('Should should fetch all leaf level element names in a hierarchy')
+  it('Should fetch all element names in a hierarchy', async () => {
+    const elementNames = await global.tm1.dimensions.hierarchies.elements.getAllNames(dimensionName, hierarchyName);
+    expect(elementNames).toEqual(['Element 1', 'Element 2']);
+  })
 
+  it('Should create a new element in a hierarchy', async () => {
+    const newElementName = 'Element 3';
+    const newElement = new HierarchyElement(newElementName, 'Numeric');
+    await global.tm1.dimensions.hierarchies.elements.create(dimensionName, hierarchyName, newElement);
+    const createdElement = await global.tm1.dimensions.hierarchies.elements.get(dimensionName, hierarchyName, newElementName);
+    expect(createdElement).toBeInstanceOf(HierarchyElement);
+    expect(createdElement.name).toEqual(newElementName);
+    expect(createdElement.type).toEqual(ElementType.Numeric);
+  })
+
+  it('Should update an element in a hierarchy', async () => {
+    const element = await global.tm1.dimensions.hierarchies.elements.get(dimensionName, hierarchyName, 'Element 3');
+    element.type = 'String';
+    await global.tm1.dimensions.hierarchies.elements.update(dimensionName, hierarchyName, element);
+    const updatedElement = await global.tm1.dimensions.hierarchies.elements.get(dimensionName, hierarchyName, 'Element 3');
+    expect(updatedElement.type).toEqual(ElementType.String);
+  })
+
+  it('Should delete an element from a hierarchy', async () => {
+    const elementToDelete = 'Element 1';
+    const elements = await global.tm1.dimensions.hierarchies.elements.getAll(dimensionName, hierarchyName);
+    const originalCount = elements.length;
+    await global.tm1.dimensions.hierarchies.elements.delete(dimensionName, hierarchyName, elementToDelete);
+    const updated = await global.tm1.dimensions.hierarchies.elements.getAll(dimensionName, hierarchyName);
+    const updatedCount = updated.length;
+    expect(updatedCount).toEqual(originalCount - 1);
+    expect(updated.find(el => el.name === elementToDelete)).toBeUndefined();
+  })
+
+  it('Should should fetch all leaf level elements in a hierarchy', async () => {
+    const elements = await global.tm1.dimensions.hierarchies.elements.getAllLeaf(dimensionName, hierarchyName); 
+    const originalCount = elements.length;
+    const newConsolName = 'Consol 1';
+    const newConsol = new HierarchyElement(newConsolName, 'Consolidated');
+    await global.tm1.dimensions.hierarchies.elements.create(dimensionName, hierarchyName, newConsol);
+    const updated = await global.tm1.dimensions.hierarchies.elements.getAllLeaf(dimensionName, hierarchyName);
+    const updatedCount = updated.length;
+    expect(updated.find(el => el.type === 'Consolidated')).toBeUndefined();
+    expect(originalCount).toEqual(updatedCount);
+  })
+
+  it('Should should fetch all leaf level element names in a hierarchy', async () => {
+    // First recreate hierarchy
+    await setup();
+    const elements = await global.tm1.dimensions.hierarchies.elements.getAllLeafNames(dimensionName, hierarchyName);
+    expect(elements).toEqual(['Element 1', 'Element 2']);
+  })
 })
