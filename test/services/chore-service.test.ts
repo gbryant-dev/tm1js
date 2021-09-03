24c8444468ca25e94b3d5c46b780f70440c2ac34
gbryant-dev <gbryant1@hotmail.co.uk>
2021-09-03 12:08:39 +0100

    Create test suite for ChoreService

---
+++ test/services/chore-service.test.ts
@@ -0,0 +1,20 @@
+
+
+describe('ChoreService', () => {
+
+
+  const prefix = 'TM1ts_test_';
+  const choreName = prefix + 'chore';
+
+  const setup = async () => {}
+  const cleanup = async () => {}
+
+  beforeAll(async () => {
+    await setup()
+  })
+
+  afterAll(async () => {
+    await cleanup()
+  })
+
+})
