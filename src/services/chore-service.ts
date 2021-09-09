84e478089d50c15af624ead5b54036a930bf8511
gbryant-dev <gbryant1@hotmail.co.uk>
2021-09-09 23:14:05 +0100

    Setup Chore service and common methods

--- src/services/chore-service.ts
+++ src/services/chore-service.ts
@@ -10,12 +10,63 @@ class ChoreService {
   }
 
   async get(choreName: string): Promise<Chore> {
-    const url = `Chores('${choreName}')?$expand=Tasks($expand=Process($select=Name,Parameters))`;
+    const url = `/api/v1/Chores('${choreName}')?$expand=Tasks($expand=Process($select=Name,Parameters))`;
     const response = await this.http.GET(url);
     return Chore.fromJson(response);
   }
 
+  async getAll(): Promise<Chore[]> {
+    const url = '/api/v1/Chores?$expand=Tasks($expand=Process($select=Name,Parameters))';
+    const response = await this.http.GET(url);
+    return response['value'].map((chore: any) => Chore.fromJson(chore))
+  }
 
+  async getAllNames(): Promise<string[]> {
+    const url = '/api/v1/Chores?$select=Name';
+    const response = await this.http.GET(url);
+    return response['value'].map((c: any) => c['Name']);
+  }
+
+  async create(chore: Chore): Promise<any> {
+    const url = `/api/v1/Chores`;
+    return await this.http.POST(url, chore.body);
+  }
+
+  async update(chore: Chore): Promise<any> {
+    const url =  `/api/v1/Chores('${chore.name}')`;
+    return await this.http.PATCH(url, chore.body);
+  }
+
+  async delete(choreName: string): Promise<any> {
+    const url = `/api/v1/Chores('${choreName}')`;
+    return await this.http.DELETE(url);
+  }
+
+  // *TODO
+  async execute(choreName: string): Promise<any> {}
+  async executeChore(choreName: string): Promise<any> {}
+
+  async activate(choreName: string): Promise<any> { 
+    const url = `/api/v1/Chores('${choreName}')/tm1.Activate`;
+    return await this.http.POST(url, null);
+  }
+
+  async deactivate(choreName: string): Promise<any> {
+    const url = `/api/v1/Chores('${choreName}')/tm1.Deactivate`;
+    return await this.http.POST(url, null);
+  }
+
+  async exists(choreName: string): Promise<boolean> {
+    try {
+      await this.http.GET(`/api/v1/Chores('${choreName}')?$select=Name`);
+      return true;
+    } catch (e) {
+      if (e.status === 404) {
+        return false
+      }
+      throw e
+    }
+  }
 
 }
 
