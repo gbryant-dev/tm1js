9957ee57f131f79550f864120e4dace19a21eb89
gbryant-dev <gbryant1@hotmail.co.uk>
2021-12-22 21:50:42 +0000

    Set up tests for ViewService

---
+++ test/services/view-service.test.ts
@@ -0,0 +1,20 @@
+
+
+describe('ViewService', () => {
+
+    const prefix = 'TM1ts_test_'
+    
+
+    const setup = async () => {}
+    const cleanup = async () => {}
+
+    beforeAll(async () => await setup())
+    afterAll(async () => await cleanup())
+
+
+    it.todo('Should fetch a mdx view')
+    it.todo('Should fetch a native view')
+    it.todo('Should fetch all views for a cube')
+    it.todo('Should create and delete a view')
+    it.todo('Should update a view')
+})
