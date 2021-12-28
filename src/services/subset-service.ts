841ba550054f84d6982c29d391a25236fa58c584
gbryant-dev <gbryant1@hotmail.co.uk>
2021-12-28 11:55:53 +0000

    Add docstrings to `SubsetService`

--- src/services/subset-service.ts
+++ src/services/subset-service.ts
@@ -2,7 +2,9 @@ import Subset from "../models/subset";
 import { fixedEncodeURIComponent } from "../utils/helpers";
 import RestService from "./rest-service";
 
-
+/**
+ * Service to handle operations on subsets in TM1
+ */
 class SubsetService {
 
   private http: RestService;
@@ -10,22 +12,57 @@ class SubsetService {
     this.http = http;
   }
 
-  async get(dimensionName: string, hierarchyName: string = null, subsetName: string, isPrivate: boolean = false) {
+  /**
+   * Fetch a single public or private subset along with its properties and elements from TM1
+   * 
+   * @param {string} dimensionName The name of the dimension
+   * @param {string} hierarchyName The name of the hierarchy
+   * @param {string} subsetName The name of the subset
+   * @param {boolean} [isPrivate=false] Private (true) or Public (false) subset. Defaults to a public subset
+   * @returns {Subset} An instance of the `Subset` model
+   */
+  
+  async get(dimensionName: string, hierarchyName: string = null, subsetName: string, isPrivate: boolean = false): Promise<Subset> {
     const subsetType = isPrivate ? 'PrivateSubsets' : 'Subsets';
     const response = await this.http.GET(`/api/v1/Dimensions('${fixedEncodeURIComponent(dimensionName)}')/Hierarchies('${fixedEncodeURIComponent(hierarchyName || dimensionName)}')/${subsetType}('${fixedEncodeURIComponent(subsetName)}')?$select=*,Alias&$expand=Hierarchy($select=Name),Elements($select=Name)`);
     return Subset.fromJson(response);
   }
 
-  async getAllNames(dimensionName: string, hierarchyName: string = null, isPrivate: boolean = false) {
+  /**
+   * Fetch the names of all public or private subsets in a hierarchy from TM1
+   * 
+   * @param dimensionName The name of the dimension
+   * @param hierarchyName The name of the hierarchy
+   * @param {boolean} [isPrivate=false] Private (true) or Public (false) subset. Defaults to a public subset
+   * @returns {string[]} An array of subset names
+   */
+
+  async getAllNames(dimensionName: string, hierarchyName: string = null, isPrivate: boolean = false): Promise<string[]> {
     const subsetType = isPrivate ? 'PrivateSubsets' : 'Subsets';
     const response = await this.http.GET(`/api/v1/Dimensions('${fixedEncodeURIComponent(dimensionName)}')/Hierarchies('${fixedEncodeURIComponent(hierarchyName || dimensionName)}')/${subsetType}?$select=Name`);
     return response['value'].map((s: any) => s['Name']);
   }
 
+  /**
+   * Create a public or private subset in TM1
+   * 
+   * @param {Subset} subset The subset to create. An instance of the `Subset` model 
+   * @param {boolean} [isPrivate=false] Private (true) or Public (false) subset. Defaults to a public subset
+   * @returns 
+   */
+
   async create(subset: Subset, isPrivate: boolean = false) {
     const subsetType = isPrivate ? 'PrivateSubsets' : 'Subsets';
     return this.http.POST(`/api/v1/Dimensions('${fixedEncodeURIComponent(subset.dimensionName)}')/Hierarchies('${fixedEncodeURIComponent(subset.hierarchyName)}')/${subsetType}`, subset.body);
   }
+
+  /**
+   * Update a public or private subset in TM1
+   * 
+   * @param {Subset} subset The subset to update. An instance of the `Subset` model
+   * @param {boolean} [isPrivate=false] Private (true) or Public (false) subset. Defaults to a public subset
+   * @returns 
+   */
 
   async update(subset: Subset, isPrivate: boolean = false) {
     const subsetType = isPrivate ? 'PrivateSubsets' : 'Subsets';
@@ -37,17 +74,47 @@ class SubsetService {
     return this.http.PATCH(`/api/v1/Dimensions('${fixedEncodeURIComponent(subset.dimensionName)}')/Hierarchies('${fixedEncodeURIComponent(subset.hierarchyName)}')/${subsetType}('${fixedEncodeURIComponent(subset.name)}')`, subset.body);
   }
 
+  /**
+   * Delete a public or private subset in TM1
+   * 
+   * @param {string} dimensionName The name of the dimension
+   * @param {string} hierarchyName The name of the hierarchy
+   * @param {string} subsetName The name of the subset
+   * @param {boolean} [isPrivate=false] Private (true) or Public (false) subset. Defaults to a public subset
+   * @returns 
+   */
+
   async delete(dimensionName: string, hierarchyName: string, subsetName: string, isPrivate: boolean = false) {
     const subsetType = isPrivate ? 'PrivateSubsets' : 'Subsets';
     return this.http.DELETE(`/api/v1/Dimensions('${fixedEncodeURIComponent(dimensionName)}')/Hierarchies('${fixedEncodeURIComponent(hierarchyName)}')/${subsetType}('${fixedEncodeURIComponent(subsetName)}')`);
   }
+
+  /**
+   * Delete all elements in a public or private subset in TM1
+   * 
+   * @param {string} dimensionName The name of the dimension
+   * @param {string} hierarchyName The name of the hierarchy
+   * @param {string} subsetName The name of the subset
+   * @param {boolean} [isPrivate=false] Private (true) or Public (false) subset. Defaults to a public subset
+   * @returns 
+   */
 
   async deleteAllElements(dimensionName: string, hierarchyName: string, subsetName: string, isPrivate: boolean = false) {
     const subsetType = isPrivate ? 'PrivateSubsets' : 'Subsets';
     return this.http.DELETE(`/api/v1/Dimensions('${fixedEncodeURIComponent(dimensionName)}')/Hierarchies('${fixedEncodeURIComponent(hierarchyName)}')/${subsetType}('${fixedEncodeURIComponent(subsetName)}')/Elements/$ref`);
   }
 
-  async exists(dimensionName: string, hierarchyName: string, subsetName: string, isPrivate: boolean = false) {
+  /**
+   * Check if a public or private subset exists in TM1
+   * 
+   * @param dimensionName The name of the dimension
+   * @param hierarchyName The name of the hierarchy
+   * @param subsetName The name of the subset
+   * @param {boolean} [isPrivate=false] Private (true) or Public (false) subset. Defaults to a public subset
+   * @returns {boolean} If the subset exists 
+   */
+
+  async exists(dimensionName: string, hierarchyName: string, subsetName: string, isPrivate: boolean = false): Promise<boolean> {
     const subsetType = isPrivate ? 'PrivateSubsets' : 'Subsets';
     try {
       await this.http.GET(`/api/v1/Dimensions('${fixedEncodeURIComponent(dimensionName)}')/Hierarchies('${fixedEncodeURIComponent(hierarchyName)}')/${subsetType}('${fixedEncodeURIComponent(subsetName)}')`);
